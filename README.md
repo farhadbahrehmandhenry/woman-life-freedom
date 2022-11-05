50fe803a3859ae93946c2d7a6b45a6cac901e643
Farhad <fbahrehmand82@gmail.com>
2022-11-05 13:00:27 -0700

    fixed animation

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # woman-life-freedom
 
 [Live Demo](https://farhadbahrehmandhenry.github.io/woman-life-freedom/index.html)               
+
+
+gsap core => creates animation
+scrollTrigger => triggers those animatiions
+
+// to
+// from
+// fromTo
+
+// to => from 0 to 500
+gsap.to('.square', {
+    duration: 5,
+    x: 500,
+    opacity: 0
+});
+
+// from => from 500 to 0
+gsap.from('.square', {
+    duration: 5,
+    x: 500,
+    opacity: 0
+});
+
+// fromTo => from red to blue
+gsap.from('.square', 
+{backgroundColor: 'red},
+{
+  backgroundColor: 'blue',
+  duration: 4
+}
+);
--- index.js
+++ index.js
@@ -11,11 +11,21 @@ var cloud1 = document.querySelector('.cloud-one');
 var cloud2 = document.querySelector('.cloud-two');
 var cloud3 = document.querySelector('.cloud-three');
 var sun = document.getElementById('sun');
+var bloodDrop = document.getElementById('blood-drop');
+var flood = document.getElementById('flood');
+var winter1 = document.querySelector('.winter1');
+var winter2 = document.querySelector('.winter2');
+var winter3 = document.querySelector('.winter3');
+var winter4 = document.querySelector('.winter4');
+var winter5 = document.querySelector('.winter5');
+var winter6 = document.querySelector('.winter6');
+var winter7 = document.querySelector('.winter7');
 
-// section two
-var iran = document.getElementById('iran-title');
-var firstParagraph = document.getElementById('first-paragraph');
-var secondParagraph = document.getElementById('second-paragraph');
+// sky
+var cloudySky = document.getElementById('cloudy-sky');
+var cloud1 = document.querySelector('.cloud1');
+var cloud2 = document.querySelector('.cloud2');
+var cloud3 = document.querySelector('.cloud3');
 
 // section three
 var sectionThree = document.getElementById('section-three');
@@ -26,105 +36,96 @@ var blood2 = document.getElementById('blood-2');
 var blood3 = document.getElementById('blood-3');
 var blood4 = document.getElementById('blood-4');
 
+var partOneTexts = document.getElementById('part-one-texts');
+var woman1 = document.querySelector('.woman1');
+var woman2 = document.querySelector('.woman2');
+var woman3 = document.querySelector('.woman3');
+var woman4 = document.querySelector('.woman4');
+var woman5 = document.querySelector('.woman5');
+var woman6 = document.querySelector('.woman6');
+var woman7 = document.querySelector('.woman7');
+var storyOneBlood1 = document.getElementById('story-one-blood1');
+var storyOneBlood2 = document.getElementById('story-one-blood2');
+
+
 // section four
 var sectionFour = document.getElementById('section-four');
-var sectionOneTitle = document.getElementById('story-one-title');
-var women = document.getElementById('women');
-var storyOneOne = document.getElementById('story-one-one');
-var storyOneTwo = document.getElementById('story-one-two');
-var storyOneThree = document.getElementById('story-one-three');
-var storyOneFour = document.getElementById('story-one-four');
-var storyOneFive = document.getElementById('story-one-five');
-var storyOneSix = document.getElementById('story-one-six');
-var storyOneSeven = document.getElementById('story-one-seven');
-var desperate = document.getElementById('desperate');
-var happy = document.querySelector('#story-one-title .changable .show');
+var sectionFourText = document.getElementById('part-two');
+var life = document.getElementById('life');
+var lifeImage = document.querySelector('.life-image');
+
+var partTwoTexts = document.getElementById('part-two-texts');
+var life1 = document.querySelector('.life1');
+var life2 = document.querySelector('.life2');
+var life3 = document.querySelector('.life3');
+var life4 = document.querySelector('.life4');
+var life5 = document.querySelector('.life5');
+var life6 = document.querySelector('.life6');
+var life7 = document.querySelector('.life7');
+var life8 = document.querySelector('.life8');
+var life9 = document.querySelector('.life9');
+var storyTwoDoll1 = document.querySelector('.story-two-doll1');
+var storyTwoDoll2 = document.querySelector('.story-two-doll2');
 
 // animations
-// var cloud1Move = gsap.to(cloud1, { scale: 1.1, duration: 9, delay: 2 });
-// var cloud2Move = gsap.to(cloud2, { scale: 1.1, duration: 7, delay: 5 });
-// var cloud3Move = gsap.to(cloud3, { scale: 1.1, duration: 11 });
+var cloud1Move = gsap.to(cloud1, { scale: 1.1, duration: 9, delay: 2 });
+var cloud2Move = gsap.to(cloud2, { scale: 1.1, duration: 7, delay: 5 });
+var cloud3Move = gsap.to(cloud3, { scale: 1.1, duration: 11 });
 
-// cloud1Move.repeat(10).yoyo(true).play();
-// cloud2Move.repeat(10).yoyo(true).play();
-// cloud3Move.repeat(10).yoyo(true).play();
+cloud1Move.repeat(10).yoyo(true).play();
+cloud2Move.repeat(10).yoyo(true).play();
+cloud3Move.repeat(10).yoyo(true).play();
 
-// gsap.to(cloud1, {
-//   x: -500,
-//   duration: 2,
-//   scrollTrigger: {
-//     trigger: sectionThree,
-//     start: "top 85%",
-//     end: "top 80%",
-//     scrub: 3,
-//     toggleActions: "play complete none none" // onEnter onLeave onEnterBack onLeaveBack
-//   }
-// });
+ScrollTrigger.create({
+  trigger: flood,
+  duration: 2,
+  start: "top 95%",
+  end: "top 30%",
+  pin: bloodDrop,
+  pinSpacing: false,
+  scrub: 1,
+});
 
-// gsap.to(cloud2, {
-//   x: 1000,
-//   duration: 5,
-//   scrollTrigger: {
-//     trigger: sectionThree,
-//     start: "top 80%",
-//     end: "top 75%",
-//     scrub: 3,
-//     toggleActions: "play complete none none"
-//   }
-// });
-
-// gsap.to(cloud3, {
-//   y: -500,
-//   duration: 5,
-//   scrollTrigger: {
-//     trigger: sectionThree,
-//     start: "top 75%",
-//     end: "top 70%",
-//     scrub: 3,
-//     toggleActions: "play complete none none"
-//   }
-// });
-
-gsap.to(firstParagraph, {
-  opacity: 1,
-  ease: true,
-  duration: 3,
+const winterTl = gsap.timeline({
   scrollTrigger: {
-    trigger: iran,
-    start: "top 60%",
-    end: "top 35%",
-    pin: firstParagraph,
-    pinSpacing: true,
-    scrub: 3,
+    trigger: flood,
+    start: "top 50%",
+    end: "bottom 60%",
+    scrub: 1,
   }
 });
 
-gsap.to(iran, {
-  opacity: 0.8,
-  duration: 5,
-  ease: true,
+winterTl
+  .to(winter1, { opacity: 1, duration: 1 })
+  .to(winter1, { opacity: 0, duration: 1 })
+  .to(winter2, { opacity: 1, duration: 1 })
+  .to(winter2, { opacity: 0, duration: 1 })
+  .to(winter3, { opacity: 1, duration: 1 })
+  .to(winter3, { opacity: 0, duration: 1 })
+  .to(winter4, { opacity: 1, duration: 1 })
+  .to(winter4, { opacity: 0, duration: 1 })
+  .to(winter5, { opacity: 1, duration: 1 })
+  .to(winter5, { opacity: 0, duration: 1 })
+  .to(winter6, { opacity: 1, duration: 1 })
+  .to(winter6, { opacity: 0, duration: 1 })
+  .to(winter7, { opacity: 1, duration: 1 })
+  .to(winter7, { opacity: 0, duration: 1 })
+
+const skyTl = gsap.timeline({
   scrollTrigger: {
-    trigger: secondParagraph,
-    start: "top 70%",
-    end: "top 20%",
-    pin: iran,
-    pinSpacing: true,
-    scrub: 3,
+    trigger: cloudySky,
+    start: "top 95%",
+    end: "top 5%",
+    scrub: 1,
   }
 });
 
-gsap.to(secondParagraph, {
-  opacity: 1,
-  ease: true,
-  duration: 3,
-  scrollTrigger: {
-    trigger: secondParagraph,
-    start: "top center",
-    end: "top 10%",
-    pin: true,
-    scrub: 3,
-  }
-});
+skyTl
+  .to(cloudySky, { backgroundColor: '#CC1100', duration: 3 })
+  .to(cloud1, { opacity: 0.8, duration: 2 })
+  .to(cloud2, { opacity: 0.8, duration: 1 })
+  .to(cloud3, { opacity: 0.8, duration: 1 })
+  .to(cloudySky, { backgroundColor: '#343636', duration: 2 })
 
 // gsap.to(sun, {
 //   scale: 3,
@@ -199,7 +200,7 @@ gsap.to(blood3, {
 });
 
 gsap.to(blood4, {
-  height: '10rem',
+  height: '30rem',
   duration: 5,
   scrollTrigger: {
     trigger: woman,
@@ -207,294 +208,131 @@ gsap.to(blood4, {
   }
 });
 
-// 1
-const storyOneOneTl = gsap.timeline({
+// ScrollTrigger.create({
+//   trigger: storyOneSeven,
+//   duration: 2,
+//   start: "top 60%",
+//   end: "bottom 60%",
+//   scrub: 1,
+//   toggleClass: { targets: happy, className: 'move' },
+//   // onUpdate: (self) => (),
+//   // there are more hooks
+//   // onEnter: () => alert('because of Reyhaneh, Down with dicatator!'),
+//   onLeave: () => {
+//     happy.innerText = 'desperate';
+//     alert('Because of Reyhaneh, Down with dicatator!')
+//   },
+//   // onEnterBack: () => (),
+//   // onLEaveBack: () => ()
+// });
+
+const womanTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: partOneTexts,
+    start: "top 40%",
+    end: "bottom 80%",
+    scrub: 1,
+    markers: true
+
+  }
+});
+
+womanTl
+  .to(woman1, { opacity: 1, duration: 2 })
+  .to(woman1, { opacity: 0, duration: 2 })
+  .to(woman2, { opacity: 1, duration: 2 })
+  // .to(storyOneBlood1, { opacity: 1, duration: 2 })
+  .to(woman2, { opacity: 0, duration: 2 })
+  .to(woman3, { opacity: 1, duration: 2 })
+  .to(woman3, { opacity: 0, duration: 2 })
+  .to(woman4, { opacity: 1, duration: 2 })
+  .to(woman4, { opacity: 0, duration: 2 })
+  .to(woman5, { opacity: 1, duration: 2 })
+  .to(woman5, { opacity: 0, duration: 2 })
+  .to(woman6, { opacity: 1, duration: 2 })
+  .to(woman6, { opacity: 0, duration: 2 })
+  .to(woman7, { opacity: 1, duration: 2 })
+  .to(storyOneBlood2, { opacity: 1, duration: 2 })
+  .to(woman7, { opacity: 0, duration: 2 })
+
+gsap.to(".parallax-blood", {
+  scrollTrigger: {
+    trigger: partOneTexts,
+    scrub: true
+  },
+  y: (i, target) => -ScrollTrigger.maxScroll(window) * target.dataset.speed,
+  ease: "none"
+});
+
+gsap.to(sectionFourText, {
+  opacity: 0.9,
+  duration: 2,
   scrollTrigger: {
     trigger: sectionFour,
-    start: "bottom 75%",
-    pin: sectionOneTitle,
-    scrub: 1,
+    start: "25% center",
+    end: "25% 25%",
+    scrub: 3,
   }
 });
 
-storyOneOneTl
-  .to(storyOneOne, { opacity: 0.8, duration: 2 })
-  .to(storyOneOne, { opacity: 0, duration: 1 })
-
-// 2
-const storyOneTwoTl = gsap.timeline({
+gsap.to(life, {
+  opacity: 0.9,
+  scale: 1.2,
+  duration: 2,
   scrollTrigger: {
-    trigger: storyOneOne,
-    start: "bottom center",
-    pin: sectionOneTitle,
-    scrub: 1,
+    trigger: sectionFour,
+    start: "35% center",
+    end: "35% 25%",
+    scrub: 3,
   }
 });
 
-storyOneTwoTl.to(storyOneTwo, { opacity: 0.8, duration: 1 })
-  .to(storyOneTwo, { opacity: 0, duration: 1 })
-
-// 3
-const storyOneThreeTl = gsap.timeline({
+gsap.to(lifeImage, {
+  opacity: 0.9,
+  scale: 1.2,
+  duration: 2,
   scrollTrigger: {
-    trigger: storyOneTwo,
-    start: "bottom center",
-    pin: sectionOneTitle,
-    scrub: 1,
+    trigger: sectionFour,
+    start: "45% center",
+    end: "45% 25%",
+    scrub: 3,
   }
 });
 
-storyOneThreeTl.to(storyOneThree, { opacity: 0.8, duration: 1 })
-  .to(storyOneThree, { opacity: 0, duration: 1 })
-
-// 4
-const storyOneFourTl = gsap.timeline({
+const lifeTl = gsap.timeline({
   scrollTrigger: {
-    trigger: storyOneThree,
-    start: "bottom center",
-    pin: sectionOneTitle,
+    trigger: partTwoTexts,
+    start: "top 40%",
+    end: "bottom 60%",
     scrub: 1,
   }
 });
 
-storyOneFourTl.to(storyOneFour, { opacity: 0.8, duration: 1 })
-  .to(storyOneFour, { opacity: 0, duration: 1 })
+lifeTl
+  .to(life1, { opacity: 1, duration: 2 })
+  .to(life1, { opacity: 0, duration: 2 })
+  .to(life2, { opacity: 1, duration: 2 })
+  .to(life2, { opacity: 0, duration: 2 })
+  .to(life3, { opacity: 1, duration: 2 })
+  .to(life3, { opacity: 0, duration: 2 })
+  .to(life4, { opacity: 1, duration: 2 })
+  .to(life4, { opacity: 0, duration: 2 })
+  .to(life5, { opacity: 1, duration: 2 })
+  .to(life5, { opacity: 0, duration: 2 })
+  .to(life6, { opacity: 1, duration: 2 })
+  .to(life6, { opacity: 0, duration: 2 })
+  .to(life7, { opacity: 1, duration: 2 })
+  .to(life7, { opacity: 0, duration: 2 })
+  .to(life8, { opacity: 1, duration: 2 })
+  .to(life8, { opacity: 0, duration: 2 })
+  .to(life9, { opacity: 1, duration: 2 })
+  .to(life9, { opacity: 0, duration: 2 })
 
-// 5
-const storyOneFiveTl = gsap.timeline({
+gsap.to(".parallax-bg", {
   scrollTrigger: {
-    trigger: storyOneFour,
-    start: "bottom center",
-    pin: sectionOneTitle,
-    scrub: 1,
-  }
-});
-
-storyOneFiveTl.to(storyOneFive, { opacity: 0.8, duration: 1 })
-  .to(storyOneFive, { opacity: 0, duration: 1 })
-
-// 6
-const storyOneSixTl = gsap.timeline({
-  scrollTrigger: {
-    trigger: storyOneFive,
-    start: "bottom center",
-    pin: sectionOneTitle,
-    scrub: 1,
-  }
-});
-
-storyOneSixTl.to(storyOneSix, { opacity: 0.8, duration: 1 })
-  .to(storyOneSix, { opacity: 0, duration: 1 })
-
-// 7
-const storyOneSevenTl = gsap.timeline({
-  scrollTrigger: {
-    trigger: storyOneSix,
-    start: "bottom center",
-    pin: sectionOneTitle,
-    scrub: 1,
-  }
-});
-
-storyOneSevenTl.to(storyOneSeven, { opacity: 0.8, duration: 1 })
-  .to(storyOneSeven, { opacity: 0, duration: 1 })
-
-// gsap.to(happy, {
-//   top: "-20%",
-//   scrollTrigger: {
-//     trigger: storyOneTwo,
-//     start: "top 85%",
-//     end: "top 40%",
-//     scrub: 1,
-//   }
-// });
-
-// gsap.to(desperate, {
-//   top: "80%",
-//   scrollTrigger: {
-//     trigger: storyOneTwo,
-//     start: "top 85%",
-//     end: "top 40%",
-//     scrub: 1,
-//     // toggleClass: 'hide',
-//   }
-// });
-
-// gsap.to(happy, {
-//   top: "-40%",
-//   scrollTrigger: {
-//     trigger: storyOneThree,
-//     start: "top 85%",
-//     end: "top 40%",
-//     scrub: 1,
-//   }
-// });
-
-// gsap.to(desperate, {
-//   top: "60%",
-//   scrollTrigger: {
-//     trigger: storyOneThree,
-//     start: "top 85%",
-//     end: "top 40%",
-//     scrub: 1,
-//   }
-// });
-
-// gsap.to(happy, {
-//   top: "-60%",
-//   scrollTrigger: {
-//     trigger: storyOneFour,
-//     start: "top 85%",
-//     end: "top 40%",
-//     scrub: 1,
-//   }
-// });
-
-// gsap.to(desperate, {
-//   top: "40%",
-//   scrollTrigger: {
-//     trigger: storyOneFour,
-//     start: "top 85%",
-//     end: "top 40%",
-//     scrub: 1,
-//   }
-// });
-
-// gsap.to(happy, {
-//   top: "-80%",
-//   scrollTrigger: {
-//     trigger: storyOneFive,
-//     start: "top 85%",
-//     end: "top 40%",
-//     scrub: 1,
-//   }
-// });
-
-// gsap.to(desperate, {
-//   top: "20%",
-//   scrollTrigger: {
-//     trigger: storyOneFive,
-//     start: "top 85%",
-//     end: "top 40%",
-//     scrub: 1,
-//   }
-// });
-
-// gsap.to(happy, {
-//   top: "-80%",
-//   scrollTrigger: {
-//     trigger: storyOneSix,
-//     start: "top 85%",
-//     end: "top 40%",
-//     scrub: 1,
-//   }
-// });
-
-// gsap.to(desperate, {
-//   top: "20%",
-//   scrollTrigger: {
-//     trigger: storyOneSix,
-//     start: "top 85%",
-//     end: "top 40%",
-//     scrub: 1,
-//   }
-// });
-
-// gsap.to(happy, {
-//   top: "-100%",
-//   scrollTrigger: {
-//     trigger: storyOneSeven,
-//     start: "top 85%",
-//     end: "top 40%",
-//     scrub: 1,
-//   }
-// });
-
-// gsap.to(desperate, {
-//   top: "0",
-//   scrollTrigger: {
-//     trigger: storyOneSeven,
-//     start: "top 85%",
-//     end: "top 40%",
-//     scrub: 1,
-//   }
-// });
-
-// standalone toggleClass
-// ScrollTrigger.create({
-//   trigger: storyOneOne,
-//   duration: 2,
-//   start: "bottom 70%",
-//   scrub: 1,
-//   toggleClass: 'dark',
-// });
-
-ScrollTrigger.create({
-  trigger: storyOneOne,
-  duration: 2,
-  start: "bottom 80%",
-  end: "bottom 75%",
-  scrub: 1,
-  toggleClass: { targets: happy, className: 'blink' },
-});
-
-ScrollTrigger.create({
-  trigger: storyOneTwo,
-  duration: 2,
-  start: "bottom 80%",
-  end: "bottom 65%",
-  scrub: 1,
-  toggleClass: { targets: happy, className: 'blink' },
-});
-
-ScrollTrigger.create({
-  trigger: storyOneThree,
-  duration: 2,
-  start: "bottom 80%",
-  end: "bottom 65%",
-  scrub: 1,
-  toggleClass: { targets: happy, className: 'blink' },
-});
-
-ScrollTrigger.create({
-  trigger: storyOneFour,
-  duration: 2,
-  start: "bottom 80%",
-  end: "bottom 65%",
-  scrub: 1,
-  toggleClass: { targets: happy, className: 'blink' },
-});
-
-ScrollTrigger.create({
-  trigger: storyOneFive,
-  duration: 2,
-  start: "bottom 80%",
-  end: "bottom 65%",
-  scrub: 1,
-  toggleClass: { targets: happy, className: 'blink' },
-});
-
-ScrollTrigger.create({
-  trigger: storyOneSix,
-  duration: 2,
-  start: "bottom 80%",
-  end: "bottom 65%",
-  scrub: 1,
-  toggleClass: { targets: happy, className: 'blink' },
-});
-
-ScrollTrigger.create({
-  trigger: storyOneSeven,
-  duration: 2,
-  start: "top 60%",
-  end: "bottom 60%",
-  scrub: 1,
-  toggleClass: { targets: happy, className: 'move' },
-  markers: true,
-  // onUpdate: (self) => (),
-  // there are more hooks
-  // onEnter: () => alert('because of Reyhaneh, Down with dicatator!'),
-  onLeave: () => alert('Because of Reyhaneh, Down with dicatator!'),
-  // onEnterBack: () => (),
-  // onLEaveBack: () => ()
+    trigger: partTwoTexts,
+    scrub: true
+  },
+  y: (i, target) => -ScrollTrigger.maxScroll(window) * target.dataset.speed,
+  ease: "none"
 });
